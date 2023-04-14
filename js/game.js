9bd95a23f00a024b29fea941cb7ad59d89111fba
alyssia-fung <124389558+alyssia-fung@users.noreply.github.com>
2023-04-14 16:46:41 +0000

    sync

---
+++ js/game.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2023 Alyssia Fung. All rights reserved.
+//
+// Created by: Alyssia Fung
+// Created on: Mar 2023
+/* global Phaser */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  backgroundColor: 0x5f6e7a,
+}
+const game = new Phaser.Game(config)
+console.log(game)
